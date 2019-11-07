cc66ed997f8c1dabfbf49012fde147a8b9cb425b
Kanin <im@kanin.dev>
2019-11-07 12:00:19 -0500

    Might be this causing it to never stop? Honestly not sure anymore..

--- utils/Database/Prefix.py
+++ utils/Database/Prefix.py
@@ -35,7 +35,7 @@ async def add_prefix(bot, guild_id: str, prefix: str):
 
 
 async def get_prefixes(bot, msg):
-    if msg.channel.id != 483061332766097419:
+    if bot.testing:
         return "n!"
     prefixes = [bot.user.mention + " "]
     prefixes.extend(bot.config()["prefixes"]["debug"] if bot.debug else bot.config()["prefixes"]["main"])
