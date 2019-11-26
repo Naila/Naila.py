2ca19efe90818a027085a9ecbb0d4e736baca0ee
Kanin <im@kanin.dev>
2019-11-26 12:50:42 -0500

    Updating circleci to hopefully be faster(?)

---
+++ modules/Cogs/RoleInfo.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+__author__ = "Kanin"
+__date__ = 11 / 22 / 2019
+__copyright__ = "Copyright 2019, Naila"
+__credits__ = ["Kanin"]
+__license__ = "GPL v3.0"
+__version__ = "0.0.1"
+__maintainer__ = "Kanin"
+__email__ = "im@kanin.dev"
+__status__ = "Development"
+
+
+class RoleInfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(RoleInfo(bot))
