2869649b4c6190190c068166786657937ddad293
MadamJazzy <22993774+MadamJazzy@users.noreply.github.com>
2020-01-26 03:57:13 -0500

    Database for Banlist

---
+++ utils/database/BanSettings.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from datetime import datetime
+from utils.ctx import CustomContext
+
+
+__author__ = "Sapphire"
+__date__ = "01/22/2022"
+__copyright__ = "Copyright 2020, Sapphire"
+__credits__ = ["Sapphire"]
+__license__ = "GPL v3.0"
+__version__ = "1.0.1"
+__maintainer__ = "Sapphire"
+__email__ = "Sapphire@hardinserver.com"
+__status__ = "Production"
+
+
+@dataclass
+class Banlist:
+    ctx: CustomContext
+    logch = 667894488341020693
+    reportchan = 667894451644923906
+    judgement = 667894473174286367
+    ds_server = 294505571317710849
+    banlist_judge = 667897608379039764
+    singleapi = ""
+    listapi = ""
+
+    async def report(self, messageid, user, reason, proof, reporter):
+        ctx = self.ctx
+        await ctx.pool.execute("INSERT INTO reports(messageid, id, reason, proof, reporter) VALUES($1, $2, $3, $4, $5)", messageid, user.id, reason, proof, reporter)
+
+    async def callreport(self, messageid):
+        ctx = self.ctx
+        data = await ctx.pool.fetch("SELECT * FROM reports WHERE messageid=$1", messageid)
+        return data
+
+    async def approve(self, user, reason, proof, reporter):
+        ctx = self.ctx
+        date = datetime.now().strftime(self.bot.config()['time_format'])
+        await ctx.pool.execute("INSERT INTO bans(id, reason, proof, reporter, date) VALUES($1, $2, $3, $4, $5)", user.id, reason, proof, reporter, date)
+
+    async def reject(self, messageid):
+        ctx = self.ctx
+        await ctx.pool.execute("DELETE FROM reports where messageid=$1", messageid)
+
+    async def lookup(self, user):
+        ctx = self.ctx
+        data = await ctx.pool.fetch("SELECT * FROM bans WHERE id=$1", user.id)
+        return data
+
+    async def revoke(self, user):
+        ctx = self.ctx
+        await ctx.pool.execute("DELETE FROM reports where id=$1", user)
