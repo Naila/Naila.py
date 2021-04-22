9041ec24b3610cc5024fe325d60e092395fa68bd
Kanin <im@kanin.dev>
2021-04-22 10:30:26 -0400

    Small changes to bot checks

--- utils/checks/bot_checks.py
+++ utils/checks/bot_checks.py
@@ -1,5 +1,13 @@
 import discord
+from discord.abc import GuildChannel
 from discord.ext import commands
+
+
+def can_reply(ctx: commands.Context = None, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    if not can_read_history(channel=channel):
+        return False
+    return can_send(channel=channel)
 
 
 def check_hierarchy(ctx: commands.Context, role: discord.Role):
@@ -7,61 +15,67 @@ def check_hierarchy(ctx: commands.Context, role: discord.Role):
 
 
 def can_manage_user(ctx: commands.Context, user: discord.Member):
-    return ctx.guild.me.top_role > user.top_role
+    return check_hierarchy(ctx, user.top_role)
 
 
-def can_send(ctx: commands.Context, channel: discord.TextChannel = None):
-    channel = ctx.channel if not channel else channel
+def can_send(ctx: commands.Context = None, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
     if isinstance(channel, discord.DMChannel):
         return True
     return ctx.guild.me.permissions_in(channel).send_messages
 
 
-def can_read_history(ctx: commands.Context, channel: discord.TextChannel = None):
+def can_read_history(ctx: commands.Context = None, channel: discord.TextChannel = None):
     channel = channel or ctx.channel
     if isinstance(channel, discord.DMChannel):
         return True
     return ctx.guild.me.permissions_in(channel).read_message_history
 
 
-def can_embed(ctx: commands.Context, channel: discord.TextChannel = None):
-    channel = ctx.channel if not channel else channel
+def can_embed(ctx: commands.Context = None, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    if isinstance(channel, discord.DMChannel):
+        return True
     return ctx.guild.me.permissions_in(channel).embed_links
 
 
-def can_react(ctx: commands.Context, channel: discord.TextChannel = None):
-    channel = ctx.channel if not channel else channel
+def can_react(ctx: commands.Context = None, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    if isinstance(channel, discord.DMChannel):
+        return True
     return ctx.guild.me.permissions_in(channel).add_reactions
 
 
-def can_delete(ctx: commands.Context):
-    if not ctx.guild:
+def can_delete(ctx: commands.Context, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    if isinstance(channel, discord.DMChannel):
         return False
-    return ctx.guild.me.permissions_in(ctx.channel).manage_messages
+    return ctx.guild.me.permissions_in(channel).manage_messages
 
 
 def can_ban(ctx: commands.Context, member: discord.Member):
     if not ctx.guild:
         return False
-    return ctx.guild.me.permissions_in(ctx.channel).ban_members and check_hierarchy(ctx, member.top_role)
+    return ctx.guild.me.permissions_in(ctx.channel).ban_members and can_manage_user(ctx, member)
 
 
 def can_kick(ctx: commands.Context, member: discord.Member):
     if not ctx.guild:
         return False
-    return ctx.guild.me.permissions_in(ctx.channel).kick_members and check_hierarchy(ctx, member.top_role)
+    return ctx.guild.me.permissions_in(ctx.channel).kick_members and can_manage_user(ctx, member)
 
 
 def can_edit_user_nick(ctx: commands.Context, member: discord.Member):
     if not ctx.guild:
         return False
-    return ctx.guild.me.permissions_in(ctx.channel).manage_nicknames and check_hierarchy(ctx, member.top_role)
+    return ctx.guild.me.permissions_in(ctx.channel).manage_nicknames and can_manage_user(ctx, member)
 
 
-def can_edit_channel(ctx: commands.Context):
-    if not ctx.guild:
+def can_edit_channel(ctx: commands.Context = None, channel: GuildChannel = None):
+    channel = channel or ctx.channel
+    if not isinstance(channel, GuildChannel):
         return False
-    return ctx.guild.me.permissions_in(ctx.channel).manage_channels
+    return ctx.guild.me.permissions_in(channel).manage_channels
 
 
 def can_edit_role(ctx: commands.Context, role: discord.Role):
